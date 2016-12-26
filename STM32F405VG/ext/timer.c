ee0fb1c1e03af9d4c33c7f1912fa06dc81b7cc0c
Rex <holyrexcheng@gmail.com>
2016-12-26 16:10:35 +0800

    Timer should be done, but I NEED BOARDgit add -A!

--- STM32F405VG/ext/timer.c
+++ STM32F405VG/ext/timer.c
@@ -3,6 +3,11 @@
 static TimerActionStruct actions[TIMER_SIZE] = {0};
 static u16 index = 0;
 static u16 size = 0;
+static u32 currCounting = 0;
+
+static bool running = false;
+
+static volatile bool fake_irq = false;
 
 void timer_init(){
 	RCC_APB1PeriphClockCmd(TIMER_RCC, ENABLE);
@@ -25,8 +30,10 @@ void timer_init(){
 	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
 	NVIC_Init(&NVIC_InitStructure);
 	
-	TIM_ARRPreloadConfig(TIMER_TIM, ENABLE);
-	TIM_Cmd(TIMER_TIM, ENABLE);
+	TIM_ARRPreloadConfig(TIMER_TIM, DISABLE);
+	TIM_Cmd(TIMER_TIM, DISABLE);
+	running = false;
+	fake_irq =  false;
 }
 
 /**
@@ -50,8 +57,55 @@ void do_after(TimerAction action, u32 ms){
 	
 	index = (index+1)%TIMER_SIZE;
 	size++;
+	
+	if (running == false){
+		TIM_SetCounter(TIMER_TIM, 0);
+		TIM_SetAutoreload(TIMER_TIM, ms);
+		TIM_Cmd(TIMER_TIM, ENABLE);
+		currCounting = ms;
+		
+		fake_irq = true;
+		TIM_GenerateEvent(TIMER_TIM, TIM_EventSource_Update);
+		
+	}else if (currCounting - TIM_GetCounter(TIMER_TIM) > ms){
+		//u32 old_count = TIM_GetCounter(TIMER_TIM);
+		//TIM_SetCounter(TIMER_TIM, old_count);
+		TIM_SetAutoreload(TIMER_TIM, currCounting+ms);
+		currCounting = currCounting+ms;
+		
+		fake_irq = true;
+		TIM_GenerateEvent(TIMER_TIM, TIM_EventSource_Update);
+	}
 }
 
 void TIMER_IRQ_HANDLER(void){
-	
+	if (TIM_GetITStatus(TIMER_TIM, TIM_IT_Update) != RESET){
+		TIM_ClearITPendingBit(TIMER_TIM, TIM_IT_Update);
+		if (!fake_irq){
+			for (u16 i=0; i<TIMER_SIZE; i++){
+				if (actions[i].action != 0){
+					//If such function exist
+					if (actions[i].ms <= currCounting){
+						//Execute function when time's up
+						actions[i].action();
+						
+						//Remove the function
+						size--;
+						actions[i].action = 0;
+					}else{
+						//Otherwise keep counting
+						actions[i].ms -= currCounting;
+					}
+				}
+			}
+			
+			if (size == 0){
+				TIM_Cmd(TIMER_TIM, DISABLE);
+				running = false;
+				index = 0;
+			}
+		}else{
+			fake_irq = false;
+		}
+	}
 }
--- STM32F405VG/ext/timer.h
+++ STM32F405VG/ext/timer.h
@@ -2,6 +2,7 @@
 #define	_TIMER_H
 
 #include "stm32f4xx_tim.h"
+#include <stdbool.h>
 
 #define TIMER_SIZE 30 //Max number of actions that can be pended
 
@@ -10,8 +11,8 @@
 #define TIMER_IRQ 				TIM7_IRQn
 #define TIMER_IRQ_HANDLER	TIM7_IRQHandler
 
-//First /4 because APB1, then /4 by ClockDivision, /1000 because in ms, so 1ms = 1 count
-#define TIMER_PRESCALER (SystemCoreClock/4/4/1000) 
+//First /4 because APB1, then /4 by ClockDivision, /1000 because in 10 ms, so 1ms = 1 count
+#define TIMER_PRESCALER (SystemCoreClock/4/4/100) 
 
 typedef void(*TimerAction)(void);
 
@@ -23,7 +24,7 @@ typedef struct{
 /**
 * Register a event call that will happen after some time.
 * @param action: The function to be called
-* @param ms: The time to be waited
+* @param ms: The time to be waited (0~655350) Scaled up by 10
 */
 void do_after(TimerAction action, u32 ms);
 

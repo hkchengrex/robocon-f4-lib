d37bcfe9bb97d8339782bdeff074c6d126909cc6
Rex Cheng <hkchengad@connect.ust.hk>
2017-01-01 22:11:40 +0800

    lol just change the type cast can solve the Sqrt problem

--- STM32F405VG/core/approx_math.c
+++ STM32F405VG/core/approx_math.c
@@ -178,7 +178,7 @@ s16 int_arc_tan2(s32 y, s32 x)
   *					average 0.000000559339% deviation
   *					Larger input generally results in smaller output error
   * @param  v:	Input limited to 2^31 by variable type
-  * @retval Scaled value of 1000*sqrt(v)
+  * @retval Scaled value of 1024*sqrt(v)
 	*	@attention		To CS members: 		Don't bother changing things not understandable unless O(f{n}) < O(1)
 	*								To CPEG members:	Float is used here only because it dramatically shortens sqrt 
 	*																	time compared to other implementations in Int, try proving the
@@ -204,9 +204,9 @@ u32 Sqrt(s32 v)
 	u.f = v;
 	u.tmp = (u32)(0x233b4000 + (u.tmp >> 1));
 	u.tmp = (u32)u.f;
-	u.tmp = ((uint64_t)u.tmp + v*16384/u.tmp + 1)/2;
-	u.tmp = ((uint64_t)u.tmp + v*16384/u.tmp + 1)/2;
-	return u.tmp * 1000 / 128;
+	u.tmp = (u.tmp + (uint64_t)v*16384/u.tmp + 1)/2;
+	u.tmp = (u.tmp + (uint64_t)v*16384/u.tmp + 1)/2;
+	return u.tmp * 8;
 }
 
 
